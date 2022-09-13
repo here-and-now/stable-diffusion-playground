914bfb6dc98f6ed68c271792586ac8c34c15be18
os <jochmann.maximilian@gmail.com>
2022-09-13 21:28:09 +0200

    txt2img as a function

--- optimized_img2img.py
+++ optimized_img2img.py
@@ -56,125 +56,29 @@ def load_img(path, h0, w0):
 config = "optimizedSD/v1-inference.yaml"
 ckpt = "models/ldm/stable-diffusion-v1/model.ckpt"
 
-parser = argparse.ArgumentParser()
+def run_i
 
-parser.add_argument(
-    "--prompt", type=str, nargs="?", default="a painting of a virus monster playing guitar", help="the prompt to render"
-)
-parser.add_argument("--outdir", type=str, nargs="?", help="dir to write results to", default="outputs/img2img-samples")
-parser.add_argument("--init-img", type=str, nargs="?", help="path to the input image")
-
-parser.add_argument(
-    "--skip_grid",
-    action="store_true",
-    help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
-)
-parser.add_argument(
-    "--skip_save",
-    action="store_true",
-    help="do not save individual samples. For speed measurements.",
-)
-parser.add_argument(
-    "--ddim_steps",
-    type=int,
-    default=50,
-    help="number of ddim sampling steps",
-)
-
-parser.add_argument(
-    "--ddim_eta",
-    type=float,
-    default=0.0,
-    help="ddim eta (eta=0.0 corresponds to deterministic sampling",
-)
-parser.add_argument(
-    "--n_iter",
-    type=int,
-    default=1,
-    help="sample this often",
-)
-parser.add_argument(
-    "--H",
-    type=int,
-    default=None,
-    help="image height, in pixel space",
-)
-parser.add_argument(
-    "--W",
-    type=int,
-    default=None,
-    help="image width, in pixel space",
-)
-parser.add_argument(
-    "--strength",
-    type=float,
-    default=0.75,
-    help="strength for noising/unnoising. 1.0 corresponds to full destruction of information in init image",
-)
-parser.add_argument(
-    "--n_samples",
-    type=int,
-    default=5,
-    help="how many samples to produce for each given prompt. A.k.a. batch size",
-)
-parser.add_argument(
-    "--n_rows",
-    type=int,
-    default=0,
-    help="rows in the grid (default: n_samples)",
-)
-parser.add_argument(
-    "--scale",
-    type=float,
-    default=7.5,
-    help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
-)
-parser.add_argument(
-    "--from-file",
-    type=str,
-    help="if specified, load prompts from this file",
-)
-parser.add_argument(
-    "--seed",
-    type=int,
-    default=None,
-    help="the seed (for reproducible sampling)",
-)
-parser.add_argument(
-    "--device",
-    type=str,
-    default="cuda",
-    help="CPU or GPU (cuda/cuda:0/cuda:1/...)",
-)
-parser.add_argument(
-    "--unet_bs",
-    type=int,
-    default=1,
-    help="Slightly reduces inference time at the expense of high VRAM (value > 1 not recommended )",
-)
-parser.add_argument(
-    "--turbo",
-    action="store_true",
-    help="Reduces inference time on the expense of 1GB VRAM",
-)
-parser.add_argument(
-    "--precision", type=str, help="evaluate at this precision", choices=["full", "autocast"], default="autocast"
-)
-parser.add_argument(
-    "--format",
-    type=str,
-    help="output image format",
-    choices=["jpg", "png"],
-    default="png",
-)
-parser.add_argument(
-    "--sampler",
-    type=str,
-    help="sampler",
-    choices=["ddim"],
-    default="ddim",
-)
-opt = parser.parse_args()
+    outdir="outputs/img2img-samples")
+    init-img=None,
+    skip_grid=False,
+    skip_save=False,
+    ddim_steps=50,
+    ddim_eta=0.0,
+    n_iter=1,
+    H = None,
+    W=None,
+    strength=0.75,
+    n_samples=5,
+    n_rows=1,
+    scale=7.5,
+    from-file=False,
+    seed=None,
+    device="cuda",
+    unet_bs=1,
+    turbo=False,
+    precision="autocast"
+    format="png",
+    sampler="ddim",
 
 tic = time.time()
 os.makedirs(opt.outdir, exist_ok=True)
