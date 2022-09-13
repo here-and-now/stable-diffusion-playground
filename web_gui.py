2bb9808106ae5cad2ad5257148e951952fde14cb
os <jochmann.maximilian@gmail.com>
2022-09-13 16:55:13 +0200

    file input and b64 decode added

--- web_gui.py
+++ web_gui.py
@@ -1,5 +1,5 @@
-# display images from file system with bokeh
-
+import base64
+import io
 import os
 import sys
 import time
@@ -8,52 +8,63 @@ import numpy as np
 
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, CustomJS, Slider, Button, Div, DataTable, TableColumn, NumberFormatter
+from bokeh.models import ColumnDataSource, CustomJS, Slider, Button, Div, DataTable, TableColumn, NumberFormatter, FileInput, RangeSlider, Select
 from bokeh.plotting import figure, show, output_file
 from bokeh.models.widgets import Tabs, Panel
-from bokeh.models.widgets import TextInput, Select, CheckboxGroup, RadioGroup, Toggle, Paragraph, Div, Button, DataTable, TableColumn, NumberFormatter
-from bokeh.models.widgets import CheckboxButtonGroup, RadioButtonGroup, MultiSelect, Select, AutocompleteInput, MultiChoice, CheckboxGroup, Toggle, Dropdown
-from bokeh.models.widgets import Button, DataTable, TableColumn, NumberFormatter, Paragraph, Div, PreText, Select, MultiSelect, CheckboxGroup, CheckboxButtonGroup, RadioButtonGroup, Toggle, Dropdown, AutocompleteInput, MultiChoice, TextInput
 
 from PIL import Image
 
-# get list of images
-image_list = glob.glob('imgs/*.png')
-images = [Image.open(x).convert('RGBA') for x in image_list]
 
-image =images[0]
-xdim, ydim = image.size
-print("Dimensions: ({xdim}, {ydim})".format(**locals()))
+def image_array(image):
+    xdim, ydim = image.size
+    print("Dimensions: ({xdim}, {ydim})".format(**locals()))
+    # Create an array representation for the image `img`, and an 8-bit "4
+    # layer/RGBA" version of it `view`.
+    img = np.empty((ydim, xdim), dtype=np.uint32)
+    view = img.view(dtype=np.uint8).reshape((ydim, xdim, 4))
+    # Copy the RGBA image into view, flipping it so it comes right-side up
+    # with a lower-left origin
+    view[:, :, :] = np.flipud(np.asarray(image))
+    return img, view, xdim, ydim
 
-# Create an array representation for the image `img`, and an 8-bit "4
-# layer/RGBA" version of it `view`.
-img = np.empty((ydim, xdim), dtype=np.uint32)
-view = img.view(dtype=np.uint8).reshape((ydim, xdim, 4))
-
-# Copy the RGBA image into view, flipping it so it comes right-side up
-# with a lower-left origin
-view[:,:,:] = np.flipud(np.asarray(image))
-
-# Display the 32-bit RGBA image
-dim = max(xdim, ydim)
-fig = figure(title="Lena",
-             x_range=(0,dim), y_range=(0,dim),
-             # Specifying xdim/ydim isn't quire right :-(
-             # width=xdim, height=ydim,
-             )
-
-source = ColumnDataSource(data=dict(image=[img]))
-fig.image_rgba(image='image', x=0, y=0, dw=xdim, dh=ydim, source=source)
-
-select = Select(title="Image:", value=image_list[0], options=image_list)
 
 def update_image(attrname, old, new):
     image = Image.open(select.value).convert('RGBA')
-    xdim, ydim = image.size
-    img = np.empty((ydim, xdim), dtype=np.uint32)
-    view = img.view(dtype=np.uint8).reshape((ydim, xdim, 4))
-    view[:,:,:] = np.flipud(np.asarray(image))
+    img, view, xdim, ydim = image_array(image)
     source.data = dict(image=[img])
+
+
+def import_file(attrname, old, new):
+    # file_input returns b64 encoded string
+    buf = io.BytesIO(base64.b64decode(file_input.value))
+    image = Image.open(buf).convert('RGBA')
+    img, view, xdim, ydim = image_array(image)
+    source.data = dict(image=[img])
+
+
+# get list of images
+image_list = glob.glob('imgs/*.png')
+image_list.sort()
+images = [Image.open(x).convert('RGBA') for x in image_list]
+image = images[0]
+
+img, view, xdim, ydim = image_array(image)
+source = ColumnDataSource(data=dict(image=[img]))
+
+# Figure
+dim = max(xdim, ydim)
+fig = figure(title='Stable Diffusion',
+             x_range=(0, dim), y_range=(0, dim),
+             tools='pan,wheel_zoom,box_zoom,poly_draw,reset,save'
+             )
+fig.image_rgba(image='image', x=0, y=0, dw=xdim, dh=ydim, source=source)
+
+# Select tool
+select = Select(title="Image:", value=image_list[0], options=image_list)
 select.on_change('value', update_image)
 
-curdoc().add_root(column(select, fig))
+# File input
+file_input = FileInput(accept=".png")
+file_input.on_change('value', import_file)
+
+curdoc().add_root(column(file_input, select, fig))
