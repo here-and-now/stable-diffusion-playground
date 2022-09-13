5715f780db53dace04a534f5f1fc1e33c82b0e08
os <jochmann.maximilian@gmail.com>
2022-09-13 22:02:38 +0200

    run_txt2img working

--- test_txt2img.py
+++ test_txt2img.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append('/home/os/gits/stable-diffusion/')
+sys.path.append('/home/os/gits/stable-diffusion-playground/optimizedSD/')
 from optimized_txt2img import run_txt2img
 
 
 prompt = "testen"
 n_samples = 1
 n_iter = 1
-ddim_steps = 10
+ddim_steps = 1
 ddim_eta = 0.0
 scale = 7.5
 device = "cuda"
@@ -18,11 +19,17 @@ unet_bs = 1
 turbo = False
 precision = "autocast"
 format = "png"
-sampler = "plms"
+# sampler = "plms"
+sampler = "ddim"
 seed = None
 outdir = 'output/'
+H = 256
+W = 256
 
-run_txt2img(prompt=prompt, n_samples=n_samples, n_iter=n_iter, ddim_steps=ddim_steps, ddim_eta=ddim_eta, scale=scale, device=device, skip_grid=skip_grid, skip_save=skip_save, outdir=outdir, fixed_code=fixed_code, unet_bs=unet_bs, turbo=turbo, precision=precision, format=format, sampler=sampler, seed=seed)
+f = 8 # downsampling factor
+C = 4 # latent channels
+
+run_txt2img(prompt=prompt, n_samples=n_samples, n_iter=n_iter, ddim_steps=ddim_steps, ddim_eta=ddim_eta, scale=scale, device=device, skip_grid=skip_grid, skip_save=skip_save, outdir=outdir, fixed_code=fixed_code, unet_bs=unet_bs, turbo=turbo, precision=precision, format=format, sampler=sampler, seed=seed, H=W, W=W, f=f, C=C)
 
 
 
